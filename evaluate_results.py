873538966befbdc9d7767430ff29839ee8d7f618
Juan Malagon <48437681+juanmalagon@users.noreply.github.com>
2024-01-21 20:12:18 +0100

    ADD: end-to-end solution in experiments.ipynb

---
+++ evaluate_results.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+from scipy.stats import spearmanr, pearsonr, kendalltau
+
+
+def create_evaluation_df(
+    efficiency_scores_dict: dict,
+    efficiency_score_by_design: np.ndarray,
+    dims_for_embedding_dict: dict,
+) -> pd.DataFrame:
+    """
+    Create a dataframe with the evaluation metrics for each embedding.
+    :param efficiency_scores_dict: dictionary with the efficiency scores for
+    each embedding
+    :param efficiency_score_by_design: theoretical efficiency scores
+    :param dims_for_embedding_dict: dictionary with the number of dimensions
+    for each embedding
+    :return: dataframe with the evaluation metrics for each embedding
+    """
+    print("Creating evaluation dataframe...")
+    mae_dict = {}
+    spearmanr_dict = {}
+    pearsonr_dict = {}
+    kendalltau_dict = {}
+
+    for k, v in efficiency_scores_dict.items():
+        mae_dict[k] = mean_absolute_error(y_true=efficiency_score_by_design,
+                                          y_pred=v)
+        spearmanr_dict[k] = spearmanr(
+            a=efficiency_score_by_design, b=v
+        ).statistic  # type: ignore
+        pearsonr_dict[k] = pearsonr(
+            x=efficiency_score_by_design, y=v
+        ).statistic  # type: ignore
+        kendalltau_dict[k] = kendalltau(
+            x=efficiency_score_by_design,
+            y=v,
+        ).statistic
+
+    mae_df = pd.DataFrame.from_dict(mae_dict, orient="index", columns=["mae"])
+    spearmanr_df = pd.DataFrame.from_dict(
+        spearmanr_dict, orient="index", columns=["spearmanr"]
+    )
+    pearsonr_df = pd.DataFrame.from_dict(
+        pearsonr_dict, orient="index", columns=["pearsonr"]
+    )
+    kendalltau_df = pd.DataFrame.from_dict(
+        kendalltau_dict, orient="index", columns=["kendalltau"]
+    )
+
+    dims_df = pd.DataFrame.from_dict(
+        dims_for_embedding_dict, orient="index", columns=["dims"]
+    )
+
+    evaluation_df = pd.concat(
+        [dims_df, mae_df, spearmanr_df, pearsonr_df, kendalltau_df], axis=1
+    ).sort_values(by="dims", ascending=False)
+    print("Evaluation dataframe created.")
+
+    return evaluation_df
